7b95e2de7369433fa657c75576dee9c38f602cf7
Xi Zhang <60451412+XiZhang721@users.noreply.github.com>
2022-11-09 15:02:38 +0000

    fixed bug for card

--- App/pdiotapp/app/src/main/java/com/specknet/pdiotapp/MainConnectActivity.kt
+++ App/pdiotapp/app/src/main/java/com/specknet/pdiotapp/MainConnectActivity.kt
@@ -1,8 +1,14 @@
 package com.specknet.pdiotapp
 
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.KeyEvent
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.specknet.pdiotapp.live.LiveDataActivity
+import com.specknet.pdiotapp.utils.Constants
 import kotlinx.android.synthetic.main.activity_connect_main.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -24,13 +31,57 @@ class MainConnectActivity: AppCompatActivity(){
     lateinit var hintText: TextView
     private var timer: Timer = Timer()
 
+    private var thingyOn = false
+    private var respeckOn = false
+
+    lateinit var respeckReceiver: BroadcastReceiver
+    lateinit var thingyReceiver: BroadcastReceiver
+    val filterTestRespeck = IntentFilter(Constants.ACTION_RESPECK_LIVE_BROADCAST)
+    val filterTestThingy = IntentFilter(Constants.ACTION_THINGY_BROADCAST)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect_main)
+
+        var mainContext: Context = this
+        respeckReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val action = intent.action
+                if (action == Constants.ACTION_RESPECK_LIVE_BROADCAST && !respeckOn) {
+                    respeckOn = true
+                    runOnUiThread {
+                        recycleView.adapter = RecyclerAdapter(mainContext,respeckOn ,thingyOn)
+                    }
+                }
+            }
+        }
+        val handlerThreadRespeck = HandlerThread("bgThreadRespeckLive")
+        handlerThreadRespeck.start()
+        val looperRespeck = handlerThreadRespeck.looper
+        val handlerRespeck = Handler(looperRespeck)
+        this.registerReceiver(respeckReceiver, filterTestRespeck, null, handlerRespeck)
+
+        thingyReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val action = intent.action
+                if (action == Constants.ACTION_THINGY_BROADCAST && !thingyOn) {
+                    thingyOn = true
+                    runOnUiThread {
+                        recycleView.adapter = RecyclerAdapter(mainContext,respeckOn ,thingyOn)
+                    }
+                }
+            }
+        }
+        val handlerThreadThingy = HandlerThread("bgThreadThingyLive")
+        handlerThreadThingy.start()
+        var looperThingy = handlerThreadThingy.looper
+        val handlerThingy = Handler(looperThingy)
+        this.registerReceiver(thingyReceiver, filterTestThingy, null, handlerThingy)
+
+
         layoutManager = LinearLayoutManager(this)
         recycleView.layoutManager = layoutManager
-        adapter = RecyclerAdapter(this)
+        adapter = RecyclerAdapter(this,respeckOn ,thingyOn)
         recycleView.adapter = adapter
         var bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigationView.selectedItemId = R.id.connection
@@ -57,18 +108,22 @@ class MainConnectActivity: AppCompatActivity(){
         refreshButton = findViewById(R.id.refresh_button)
         addButton = findViewById(R.id.add_button)
         hintText = findViewById(R.id.hintText)
+
         setupClickListeners()
+
     }
     private fun setupClickListeners() {
         refreshButton.setOnClickListener {
-            val intent = Intent(this, this.javaClass)
-            startActivity(intent)
+            thingyOn = false
+            respeckOn = false
+            recycleView.adapter =RecyclerAdapter(this,respeckOn ,thingyOn)
         }
 
         addButton.setOnClickListener {
             hintTimeCount()
         }
     }
+
 
     private fun showHint() {
         if(hintShowing){
@@ -96,5 +151,11 @@ class MainConnectActivity: AppCompatActivity(){
             showHint()
             timer.schedule(hideTask,3000)
         }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(respeckReceiver)
+        unregisterReceiver(thingyReceiver)
+        super.onDestroy()
     }
 }
